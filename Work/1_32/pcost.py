e4ba5bac183b2a7ae74632806dc0f959f309f295
Yibin YANG <rainforest.vista@gmail.com>
2020-08-08 16:06:45 +0200

    Exercise 1.32

---
+++ Work/1_32/pcost.py
@@ -0,0 +1,19 @@
+# Modified based on Exercise 1.31
+import csv
+
+
+def portfolio_cost(filename):
+    with open(filename, "rt") as infile:
+        csv_reader = csv.reader(infile)
+        next(csv_reader)  # skip the headers on the 1st line
+        total = 0.0
+        for portfolio in csv_reader:
+            try:
+                total += int(portfolio[1]) * float(portfolio[2])
+            except (csv.Error, ValueError):
+                print("Bad line. Skip.")
+        return total
+
+
+cost = portfolio_cost("Work/Data/portfolio.csv")
+print("Total cost", cost)
