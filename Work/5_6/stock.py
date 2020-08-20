a11e10fe2af4c7335490f2a51a4caaa63bf724a3
Yibin YANG <rainforest.vista@gmail.com>
2020-08-20 20:47:32 +0200

    Exercise 5.6

---
+++ Work/5_6/stock.py
@@ -0,0 +1,17 @@
+class Stock:
+    def __init__(self, name: str, shares: int, price: float):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def __repr__(self):
+        """The convention for __repr__() os to return a string that, when fed to eval(),
+        will create the underlying object, if possible."""
+        return f"Stock({self.name}, {self.shares}, {self.price})"
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, sold: int):
+        self.shares -= sold
