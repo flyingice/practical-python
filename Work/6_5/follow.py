da6d8b4beade6d4209731ea603192ff163e9ef0e
Yibin YANG <rainforest.vista@gmail.com>
2020-08-23 18:01:32 +0200

    Exercise 6.7

--- Work/6_5/follow.py
+++ Work/6_5/follow.py
@@ -15,10 +15,14 @@ def follow(filename: str):
 
 
 if __name__ == "__main__":
+    import report
+
+    portfolio = report.read_portfolio("Work/Data/portfolio.csv")
+
     for line in follow("Work/Data/stocklog.csv"):
         fields = line.split(",")
         name = fields[0].strip('"')
         price = float(fields[1])
         change = float(fields[4])
-        if change < 0:
+        if name in portfolio:
             print(f"{name:>10s} {price:>10.2f} {change:>10.2f}")
---
+++ Work/6_5/report.py
@@ -0,0 +1,57 @@
+# Modified based on Exercise 3.1
+
+import fileparse
+import stock
+import tableformat
+import portfolio
+
+
+def read_portfolio(filename):
+    with open(filename, "rt") as infile:
+        portfolios = fileparse.parse_csv(
+            infile, select=["name", "shares", "price"], types=[str, int, float]
+        )
+
+    portfolios = [stock.Stock(d["name"], d["shares"], d["price"]) for d in portfolios]
+    return portfolio.Portfolio(portfolios)
+
+
+def read_prices(filename):
+    with open(filename, "rt") as infile:
+        return dict(fileparse.parse_csv(infile, types=[str, float], has_headers=False))
+
+
+def make_report(portfolios: list, prices: dict):
+    report = []
+    for p in portfolios:
+        report.append((p.name, p.shares, prices[p.name], prices[p.name] - p.price,))
+
+    return report
+
+
+def print_report(report: list, formatter: tableformat.TableFormatter):
+    """Print a nicely formated table from a list"""
+    formatter.headings(["Name", "Shares", "Price", "Change"])
+    for name, shares, price, change in report:
+        rowdata = [name, str(shares), f"{price:.2f}", f"{change:.2f}"]
+        formatter.row(rowdata)
+
+
+def portfolio_report(portfolio_filename: str, prices_filename: str, fmt: str = "txt"):
+    portfolios = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolios, prices)
+    formatter = tableformat.create_formatter(fmt)
+    print_report(report, formatter)
+
+
+def main(args):
+    if len(args) != 4:
+        raise SystemExit("Invalid parameters")
+    portfolio_report(args[1], args[2], args[3])
+
+
+if __name__ == "__main__":
+    import sys
+
+    main(sys.argv)
