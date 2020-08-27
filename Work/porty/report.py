d76b1e0393c3d09f3846ee046d2ebb25fdaec2a0
Yibin YANG <rainforest.vista@gmail.com>
2020-08-27 18:51:58 +0200

    Exercise 9.1

---
+++ Work/porty/report.py
@@ -0,0 +1,54 @@
+# Modified based on Exercise 3.1
+
+from . import fileparse
+from . import tableformat
+from . import portfolio
+
+
+def read_portfolio(filename, **opts):
+    with open(filename, "rt") as lines:
+        return portfolio.Portfolio.from_csv(lines, **opts)
+
+
+def read_prices(filename):
+    with open(filename, "rt") as infile:
+        return dict(fileparse.parse_csv(infile, types=[str, float], has_headers=False))
+
+
+def make_report(portfolios: list, prices: dict):
+    report = []
+    for p in portfolios:
+        report.append((p.name, p.shares, prices[p.name], prices[p.name] - p.price,))
+
+    return report
+
+
+def print_report(report: list, formatter: tableformat.TableFormatter):
+    """Print a nicely formated table from a list"""
+    formatter.headings(["Name", "Shares", "Price", "Change"])
+    for name, shares, price, change in report:
+        rowdata = [name, str(shares), f"{price:.2f}", f"{change:.2f}"]
+        formatter.row(rowdata)
+
+
+def portfolio_report(portfolio_filename: str, prices_filename: str, fmt: str = "txt"):
+    portfolios = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = make_report(portfolios, prices)
+    formatter = tableformat.create_formatter(fmt)
+    print_report(report, formatter)
+
+
+def main(args):
+    if len(args) != 4:
+        raise SystemExit("Invalid parameters")
+    portfolio_report(args[1], args[2], args[3])
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(filename="app.log", filemode="w", level=logging.WARNING)
+    import sys
+
+    main(sys.argv)
---
+++ Work/porty/ticker.py
@@ -0,0 +1,52 @@
+from .follow import follow
+import csv
+
+
+def select_columns(rows, indices):
+    for row in rows:
+        yield [row[index] for index in indices]
+
+
+def convert_types(rows, types):
+    for row in rows:
+        yield [func(val) for func, val in zip(types, row)]
+
+
+def make_dicts(rows, headers):
+    return (dict(zip(headers, row)) for row in rows)
+
+
+def parse_stock_data(lines):
+    rows = csv.reader(lines)
+    rows = select_columns(rows, [0, 1, 4])
+    rows = convert_types(rows, [str, float, float])
+    rows = make_dicts(rows, ["name", "price", "change"])
+    return rows
+
+
+def filter_symbols(rows, names):
+    return (row for row in rows if row["name"] in names)
+
+
+def ticker(portfile, logfile, fmt):
+    import report
+    import tableformat
+
+    portfolio = report.read_portfolio(portfile)
+    rows = parse_stock_data(follow(logfile))
+    rows = filter_symbols(rows, portfolio)
+    fmt = tableformat.create_formatter(fmt)
+    fmt.headings(["Name", "Price", "Change"])
+    for row in rows:
+        fmt.row([row["name"], str(row["price"]), str(row["change"])])
+
+
+if __name__ == "__main__":
+    import report
+
+    portfolio = report.read_portfolio("Work/Data/portfolio.csv")
+    lines = follow("Work/Data/stocklog.csv")
+    rows = parse_stock_data(lines)
+    rows = filter_symbols(rows, portfolio)
+    for row in rows:
+        print(row)
