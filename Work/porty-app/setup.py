1f24499724b173e6b09e155a2532177ca5ca47ec
Yibin YANG <rainforest.vista@gmail.com>
2020-08-28 18:13:08 +0200

    Exercise 9.5

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="porty",
+    version="0.0.1",
+    author="Yibin Yang",
+    author_email="rainforest.vista@gmail.com",
+    description="Practical Python Code",
+    packages=setuptools.find_packages(),
+)
